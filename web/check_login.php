66f0f389d4ac276c838077b75b170103d9b6cf22
จักรภูมิ มณีรัตน์ < >
2016-06-03 22:48:26 +0700

    add check user and createsession

---
+++ web/check_login.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+	
+
+
+    mysql_connect("localhost","root","kks*5cvp768");
+	mysql_select_db("proj");
+	$strSQL = "SELECT * FROM member WHERE Username = '".mysql_real_escape_string($_POST['txtUsername'])."' 
+	and Password = '".mysql_real_escape_string($_POST['txtPassword'])."'";
+	$objQuery = mysql_query($strSQL);
+	$objResult = mysql_fetch_array($objQuery);
+	
+
+
+
+
+
+
+    if(!$objResult)
+	{
+			
+        echo " <h1><center> <br> ชื่อผู้ใช้ไม่ถูกต้อง <br><br> Username and Password Incorrect! </center></h1>";
+        header("location:index.php?fail=true");
+	}	
+    else
+    {
+			$_SESSION["UserID"] = $objResult["UserID"];
+			$_SESSION["Status"] = $objResult["Status"];
+
+			session_write_close();
+			
+			if($objResult["Status"] == "ADMIN")
+			{
+				header("location:admin_page.php");
+			}
+			else if($objResult["Status"] == "USER")
+			{
+				header("location:userpage.php");
+			}
+            else
+            {
+                header("location:index.php?fail=true");
+                
+
+            }
+	}
+	mysql_close();
+?>
--- web/index.php
+++ web/index.php
@@ -1,10 +1,42 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>ระบบบันทึกข้อมูลผู้ใช้เครือข่าย</title>
-  </head>
-  <body>
-    <h1>test</h1>
-  </body>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--<link rel="icon" href="../../favicon.ico">-->
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/signin.css" rel="stylesheet">
+</head>
+
+
+<body>
+    <div class="container">
+        <form class="form-signin" name="form0" method="post" action="check_login.php">
+            <h2 class="form-signin-heading">Please sign in</h2>
+            <?php
+                $laststate = $_GET['fail'];
+                if($laststate ){
+                    echo '<div class="alert alert-danger" role="alert">Username or Password are incorrect.<br>Please try again.</div>';
+                }
+            ?>
+                <label for="inputEmail" class="sr-only">User Name</label>
+                <input type="text" id="txtUsername" name="txtUsername" class="form-control" placeholder="User Name" required autofocus>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="txtPassword" name="txtPassword" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+    <!-- /container -->
+</body>
+
 </html>
